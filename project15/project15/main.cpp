6d7038a04dfdbb30c54baac11338d6ba06e90470
Nick Kurashkin <145068709+Trafficonded@users.noreply.github.com>
2023-09-19 21:12:01 +0300

    stable

--- project15/project15/main.cpp
+++ project15/project15/main.cpp
@@ -2,45 +2,60 @@
 
 using namespace std;
 
-char letter;
-long int arr[26] = {};  //массив для занесения количества определенных символов с помощью привязки к ascii
-char letters[26] = {};	//массив который нужен для выведения символов в правильной последовательности
-long int var = 0;		//количество уникальных символов (до 26)
-bool sw = true;			//для выхода из цикла после последнего символа
-int transfer = 0;        
+char letter, letter2;
+long int arr[10000] = {};
+char letters[10000] = {};
+long int var = 1;
+long int inc = 1;
+bool sw = true, sw2 = true;
+int transfer = 0;
 
 int main()
-{	
+{
 	while (sw == true)
 	{
 		letter = cin.get();
 		if (letter == '\n')
 		{
-			sw = false;                        //конец считывания символов
+			arr[var] = inc;
+			sw = false;
+			letters[var] = letter2;
+			var = var + 1;
+			inc = 1;
+			letter2 = letter;
 		}
-		else 
+		else
 		{
-			arr[int(letter) - 65] += 1;           // если символы есть - добавляет 1 к количеству символа. Номер ячейки для отправки зависит от символа
-			for (int i = 0; i < 26; i++)
+			if (sw2 == true)
 			{
-				if (letters[i] == letter)        
-					i = 26;                     
-				else if (i == 25 && letters[i] != letter)
+				sw2 = false;
+				letter2 = letter;
+			}
+			else
+			{
+				if (letter == letter2)
 				{
-					letters[var] = letter;
-					var += 1;                  //счетчик/отделитель уникальных символов от тех которые уже записаны
+					inc += 1;
+				}
+				else
+				{
+					arr[var] = inc;
+					letters[var] = letter2;
+					var = var + 1;
+					inc = 1;
+					letter2 = letter;
 				}
 
 			}
+
 		}
-		
+
 	}
-	for (int i = 0; i < var; i++)
+	for (int i = 1; i < var; i++)
 	{
-		transfer = int(letters[i]) - 65;
-		if (arr[transfer] == 1)                                           //если символ 1 - выдает без коэф.,если 2 и более - с цифрой после
+		if (arr[i] == 1)
 			cout << letters[i];
 		else
-			cout << letters[i] << arr[transfer];
+			cout << letters[i] << arr[i];
 	}
 }
